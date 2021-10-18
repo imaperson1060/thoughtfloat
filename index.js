827f5a1ebfab11329d3a1141d45b9f28c2a9829c
Ari Meisels <me@arimeisels.com>
2021-10-17 20:24:27 -0400

    make character limit 2000

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ io.on("connection", async (socket) => {
 
         if (thought.match(/^ *$/)) return socket.emit("thoughtFailed", "THOUGHT_EMPTY");
         if (filter.isProfane(thought)) return socket.emit("thoughtFailed", "THOUGHT_PROFANE");
-        if (encodeURIComponent(thought).length > 10000) return socket.emit("thoughtFailed", "THOUGHT_EXCEEDS_10000_CHARACTERS");
+        if (encodeURIComponent(thought).length > 2000) return socket.emit("thoughtFailed", "THOUGHT_EXCEEDS_10000_CHARACTERS");
 
         await query("INSERT INTO `tf`(`thought`) VALUES (?)", [ encodeURIComponent(thought) ]);
 
